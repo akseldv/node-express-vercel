d095a58be5ff082692da3028f8a6bd422612ecea
akseldv <aksldv4@gmail.com>
2024-03-23 20:05:03 +0300

    Route Simplification

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // Import packages
 const express = require("express");
-const home = require("./routes/home");
+// const home = require("./routes/home");
 
 // Middlewares
 const app = express();
 app.use(express.json());
 
 // Routes
-app.use("/home", home);
+// app.use("/home", home);
+
+app.get("/", (req, res) => {
+    res.send('Hello World')
+  });
 
 // connection
 const port = process.env.PORT || 9001;
--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,8 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly!",
-  });
-});
+// router.get("/", (req, res) => {
+//   res.send('Hello World')
+// });
 
-module.exports = router;
+// module.exports = router;
